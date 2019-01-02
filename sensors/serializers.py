f2caafb662ca8543b23be261316bf9b6afc3271c
vladimir.tarasov <vladimir.tarasov@eosda.com>
2019-01-02 16:32:59 +0200

    modify temparature display

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from sensors.models import Sensor
 
+
 class SensorSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['temperature'] = ret['temperature']*0.01
+
+        return ret
+
     class Meta:
         model = Sensor
-        fields = ('id','sensor_id','date','temperature','humidity','pressure','pm10','pm25','nco','nso2','no3','nnh3','nno2')
+        fields = ('id', 'sensor_id', 'date', 'temperature', 'humidity',
+                  'pressure', 'pm10', 'pm25', 'nco', 'nso2', 'no3', 'nnh3', 'nno2')
--- sensors/urls.py
+++ sensors/urls.py
@@ -4,10 +4,12 @@ from rest_framework.routers import DefaultRouter
 from sensors import views
 
 # Create a router and register our viewsets with it.
+router = DefaultRouter()
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^sensors/$', views.SensorList.as_view(),
         name='sensors-list'),
     url(r'^sensors/(?P<pk>\d+)/$', views.SensorDetail.as_view(),
