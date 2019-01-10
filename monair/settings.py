066147801279e3affa8c4171c3d0b6554116c5e1
vladimir.tarasov <vladimir.tarasov@eosda.com>
2019-01-10 11:40:36 +0200

    add pagination

--- monair/settings.py
+++ monair/settings.py
@@ -83,7 +83,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'monair.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
