231b639194490e2ee086e5ef7ec1c7cea0abf12b
Nastya Vishnya <stacyvishnya@gmail.com>
2019-02-12 20:48:02 +0200

    set up MiniCssExtractPlugin

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,38 +1,51 @@
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-var BundleTracker  = require('webpack-bundle-tracker');
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
+var BundleTracker  = require("webpack-bundle-tracker");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
-        app: './frontend/src/main.js'
+        app: "./frontend/src/main.js"
     },
     output: {
-        path: path.resolve('./frontend/dist/bundles/'),
-        filename: '[name]-[hash:8].js'
+        path: path.resolve("./frontend/dist/bundles/"),
+        filename: "[name]-[hash:8].js"
     },
     module:{
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
+                loader: "vue-loader",
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 exclude: /node_modules/,
             },
             {
-                test: /\.css$/,
-                use: [
-                  'vue-style-loader',
-                  'css-loader'
-                ]
-            }            
+                test: /\.(sa|sc|c)ss$/,
+                use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader",]
+            },
+
         ]
     },
     plugins: [
         new VueLoaderPlugin(),
-        new BundleTracker({filename: './webpack-stats.json'})
-
-    ]
+        new BundleTracker({filename: "./webpack-stats.json"}),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: "styles",
+              test: /\.(sa|sc|c)ss$/,
+              chunks: "all",
+              enforce: true
+            }
+          }
+        }
+      },
 }
