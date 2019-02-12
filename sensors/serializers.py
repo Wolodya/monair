0a344e82a8a7cf1732befd5b10697854115d841f
Nastya Vishnya <stacyvishnya@gmail.com>
2019-02-12 20:44:49 +0200

    update SensorSerializer to_representation() method

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -5,6 +5,7 @@ from sensors.models import Sensor
 class SensorSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super().to_representation(instance)
+        ret['date'] = instance.date.strftime("%Y-%m-%d %H:%M:%S")
         ret['temperature'] = ret['temperature']*0.01
         return ret
 
