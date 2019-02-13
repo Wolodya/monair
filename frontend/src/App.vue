c8803af0a1bd3c29aba1d18a6695411a87f5511f
Nastya Vishnya <stacyvishnya@gmail.com>
2019-02-13 15:00:40 +0200

    set up App component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,111 @@
 <template>
-  <div id="app">
-    Test
-  </div>
+  <v-app :style="cssProps">
+    <v-toolbar :color="maincolor" app>
+      <v-toolbar-title class="pa-4" id="title">Monair</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <swatches v-model="maincolor" :colors="colors" row-length="3"
+                shapes="circles" show-border popover-to="left">
+        <v-btn slot="trigger" :color="'#424242'" fab small dark>
+          <v-icon>color_lens</v-icon>
+        </v-btn>
+      </swatches>
+    </v-toolbar>
+    <v-content>
+      <Map :date="date"></Map> 
+      <template v-if='mobile'>
+        <v-dialog ref="dialog" v-model="modal" id="datepiker"
+                  :return-value.sync="date" persistent lazy full-width width="290px">
+          <v-btn fab dark slot="activator" :color='maincolor' v-model="date">
+            <v-icon>date_range</v-icon>
+          </v-btn>
+          <v-date-picker v-model="date" :color="maincolor">
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </template>
+      <template v-else>
+        <v-date-picker id="datepiker" :color="maincolor" v-model="date"></v-date-picker>
+      </template>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import Map from "./components/Map.vue";
+import Swatches from 'vue-swatches'
+
 export default {
-  name: 'App',
-  methods: {
-    mounted: function(){
-      console.log('test');
+  name: "App",
+  components: {
+    Map,
+    Swatches
+  },
+  data(){
+    return{
+      date: new Date().toISOString().substr(0, 10),
+      maincolor: 'rgb(174, 234, 0)',
+      colors:  ['#AEEA00', '#FF8F00', '#558B2F', '#00838F', '#009688', '#607D8B', ],
+      modal: false
     }
-  }
+  },
+  computed: {
+    cssProps() { return {
+        '--maincolor': this.maincolor
+      }
+    },
+    mobile(){
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return true
+        case 'sm': return true
+        case 'md': return false
+        case 'lg': return false
+        case 'xl': return false
+      }
+    }
+	}
 }
 </script>
 
-<style>
+<style lang="sass">
+@import url("https://fonts.googleapis.com/css?family=IBM+Plex+Mono");
+$main-color: var(--maincolor);
 
+html, body{
+overflow-y:hidden;
+overflow-x:hidden;
+}
+#title{
+  font-family: "IBM Plex Mono", monospace;
+  font-style: italic;
+  font-size: x-large;
+}
+#datepiker{
+  position: fixed;
+  right: 0;
+  bottom: 1%;
+}
+.v-expansion-panel__header{
+    min-height: 1px;
+}
+.v-expansion-panel__container--active > .v-expansion-panel__header{
+    background-color: $main-color !important;;
+}
+.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip,
+.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip,
+.mapboxgl-popup-anchor-top .mapboxgl-popup-tip
+{
+    border-bottom-color: $main-color !important;;
+}
+.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip,
+.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip,
+.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip
+{
+    border-top-color: $main-color !important;;
+}
+.mapboxgl-popup-content{
+    background-color: $main-color !important;
+    padding: 8px;
+}
 </style>
